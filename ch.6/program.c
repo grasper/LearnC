030559239a534aec52b6e1ff6563cb1bb3b1914e
grasper <grasper@gmail.com>
2013-08-31 00:53:33 +0400

    first

---
+++ ch.6/program.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#ifndef __cplusplus
+typedef char bool;
+#define true 1
+#define false 0
+#endif 
+
+struct list_element
+{
+	struct list_element * nxt;
+};
+typedef struct list_element list_element;
+
+typedef struct
+{
+
+}
+
+Int main()
+{
+	return 0;
+}
